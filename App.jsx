0becd793f27c097f1f4930d0966983a3b1a13c99
Yunier AP <ayalaperezyunier@hotmail.com>
2025-10-25 21:19:58 -0500

    Carga inicial

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textWhite} >Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#616161ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textWhite:{
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold"
+  }
+
+});
